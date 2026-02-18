bb5f4fe8766ff6ccb480b94beebe2d37892394d5
SlayerFluffy <wjezek@byupathway.edu>
2026-02-18 09:06:05 -0700

    Okay this is how Netlify says to fix the issue. Lets see

--- src/js/apiManager.mjs
+++ src/js/apiManager.mjs
@@ -1,6 +1,3 @@
-// API keys - Using environment variables 
-const spoonacularAPI = import.meta.env.VITE_SPOONACULAR_API_KEY;
-
 // convert response from API to JSON
 async function convertToJson(res) {
     const jsonResponse = await res.json();
@@ -14,27 +11,23 @@ async function convertToJson(res) {
 // API manager class - handles api calls
 export class APIManager {
     constructor() {
-        this.spoonacularAPIKey = spoonacularAPI;
-        this.spoonacularBaseURL = 'https://api.spoonacular.com/recipes/';
+        this.baseURL = '/.netlify/functions/spoonacular';
     }
 
-    //make API calls
+    //make API calls via netlify function
 
     async getRecipes(listIngredients, ranking) {
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': spoonacularAPI,
-            }
-        };
         const ingredientList = listIngredients.join(',+');
         const params = new URLSearchParams({
             ingredients: ingredientList,
             ranking: ranking,
-            number: 20
+            number: 20,
         });
-        return await fetch(`${this.spoonacularBaseURL}findByIngredients?` + params, options).then(convertToJson); // this is API call #1
+        const res = await fetch(`${this.baseURL}?${params.toString()}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        return convertToJson(res);
     }
 
     // Get full recipe information by ID
@@ -46,20 +39,18 @@ export class APIManager {
         } = options;
 
         const params = new URLSearchParams({
+            type: 'information',
+            recipeId: recipeId.toString(),
             includeNutrition: includeNutrition.toString(),
             addWinePairing: addWinePairing.toString(),
-            addTasteData: addTasteData.toString()
+            addTasteData: addTasteData.toString(),
         });
 
-        const requestOptions = {
+        const res = await fetch(`${this.baseURL}?${params.toString()}`, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': spoonacularAPI,
-            }
-        };
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-        const url = `${this.spoonacularBaseURL}${recipeId}/information?${params}`; // this is API call #2 as this is a separate end point and requires different params
-        return await fetch(url, requestOptions).then(convertToJson);
+        return convertToJson(res);
     }
 }
